a9b24c3025aff7a255b43938a6f6dad52369fdd2
Danilo dos Santos <danilo.souza@brisabr.com.br>
2024-06-27 18:21:21 -0300

    feat: cria novas rotas de edit create e delete

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Communication.Request;
+using MyFirstApi.Communication.Response;
 
 namespace MyFirstApi.Controllers;
 [Route("api/[controller]")]
@@ -6,9 +8,10 @@ namespace MyFirstApi.Controllers;
 public class UserController : ControllerBase
 {
     [HttpGet]
+    [Route("{id}")]
     [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-    public IActionResult Get()
+    public IActionResult Get([FromRoute] int id)
     {
         var response = new Response
         {
@@ -16,5 +19,33 @@ public class UserController : ControllerBase
             Name = "welisson"
         };
         return Ok(response);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public IActionResult Create([FromBody]RegisterUserJson request)
+    {
+        var response = new ResponseRegisterUserJson
+        {
+            Id = 1,
+            Name= "Danilo"
+        };
+        return Created(string.Empty, response);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute] int id, [FromBody] RequestUpdateUserProfileJson request)
+    {
+
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        return NoContent();
     }
 }
